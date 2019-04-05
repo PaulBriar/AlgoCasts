edfea6053db2455ff00d5aeb0ff3fe0ead28f357
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-04-05 16:22:53 -0300

    Finished Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   let newStr = str.split('').reverse().join('');
+//   return newStr;
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//   for(let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
